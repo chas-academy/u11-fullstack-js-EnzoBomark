0a5099f2cd1d6f96be470b9d5bd787afa7556a28
EnzoBomark <enzo.bomark@chasacademy.se>
2021-08-18 08:41:48 +0200

    Add errorHandler middleware

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config({ path: './config.env' });
 
 const express = require('express');
 const connectDB = require('./config/db');
+const errorHandler = require('./middleware/error');
 
 // Connect DB
 connectDB();
@@ -11,6 +12,9 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
+
+// Error Handler (Must be last piece of middleware);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 

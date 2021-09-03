47bd011f58515847161ed3eb47a5ef59a5bd2817
EnzoBomark <enzo.bomark@chasacademy.se>
2021-09-03 14:26:08 +0200

    Fix dotenv bug

--- client/next.config.example.js
+++
@@ -1,8 +0,0 @@
-// /** @type {import('next').NextConfig} */
-// module.exports = {
-//   reactStrictMode: true,
-//   env: {
-//     BASE_API: 'http://localhost:8080/api/',
-//     BASE_S3: 'https://your-bucket/',
-//   },
-// };
--- client/utils/rest/http.utils.ts
+++ client/utils/rest/http.utils.ts
@@ -1,5 +1,6 @@
 import { HttpResponse } from '@/interfaces/HttpResponse.interface';
-const API_BASE = process.env.BASE_URL;
+
+const API_BASE = process.env.BASE_API;
 
 export const http = async <T>(request: RequestInfo): Promise<HttpResponse<T>> => {
   const response: HttpResponse<T> = await fetch(request);

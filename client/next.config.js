5813c6b480fdf62bbeda99f50a1927ba1a3872c3
EnzoBomark <enzo.bomark@chasacademy.se>
2021-09-24 20:27:52 +0200

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  distDir: 'build',
   env: {
     BASE_API: process.env.BASE_API,
     BASE_S3: process.env.BASE_S3,

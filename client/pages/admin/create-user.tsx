ca131e81090ed1d142cb7e9ee349cf283e1a095a
Enzo Bomark <enzobomark@MBPsomtlhorEnzo.lan>
2021-10-07 12:58:47 +0200

    Remove Admin Create User

--- client/pages/admin/create-user.tsx
+++ client/pages/admin/create-user.tsx
@@ -12,64 +12,65 @@ import { useFetch } from '@/hooks/useFetch.hooks';
 import { useMount } from '@/hooks/useMount';
 import { useToggle } from '@/hooks/useToggle.hooks';
 import { Response } from '@/interfaces/Response.interface';
-import { AdminCreateUserSchema, Props } from '@/schemas/admin/CreateUser.schema';
+// import { AdminCreateUserSchema, Props } from '@/schemas/admin/CreateUser.schema';
 import { S } from '@/styles/pages/admin/CreateUser.style';
 import { post } from '@/utils/http.utils';
 import { resolver } from '@/utils/resolver.utils';
 
 const AdminCreateUser: NextPage = () => {
-  const router = useRouter();
-  const res = resolver<Props>(AdminCreateUserSchema);
-  const [values, setValues] = useState<Props>();
-  const [role, setRole] = useToggle(false);
+  // const router = useRouter();
+  // const res = resolver<Props>(AdminCreateUserSchema);
+  // const [values, setValues] = useState<Props>();
+  // const [role, setRole] = useToggle(false);
 
-  const { fetch, isLoading, hasError, data } = useFetch<Response>(() =>
-    post('admin/user', { ...values, role: role ? 'admin' : '' })
-  );
+  // const { fetch, isLoading, hasError, data } = useFetch<Response>(() =>
+  //   post('admin/user', { ...values, role: role ? 'admin' : '' })
+  // );
 
-  useMount(async () => await fetch(), [values]);
-  useMount(() => router.push('/admin'), [data]);
+  // useMount(async () => await fetch(), [values]);
+  // useMount(() => router.push('/admin'), [data]);
 
-  return (
-    <S.UpdateUser>
-      <Spinner isLoading={isLoading} />
-      <S.H1>Admin Create User</S.H1>
-      <Form
-        onSubmit={res.handleSubmit((e) => setValues(e))}
-        error={hasError}
-        success={data?.success}
-      >
-        <Text
-          id="name"
-          placeholder="Name"
-          error={res.formState.errors.name?.message}
-          register={res.register('name')}
-        />
-        <Text
-          id="email"
-          placeholder="Email"
-          error={res.formState.errors.email?.message}
-          register={res.register('email')}
-        />
-        <Password
-          id="password"
-          placeholder="Password"
-          error={res.formState.errors.password?.message}
-          register={res.register('password')}
-        />
-        <Password
-          id="passwordConf"
-          placeholder="Password Confirmation"
-          error={res.formState.errors.passwordConf?.message}
-          register={res.register('passwordConf')}
-        />
-        <Checkbox value={role} onClick={() => setRole()}>
-          Admin
-        </Checkbox>
-        <S.Submit>Create User</S.Submit>
-      </Form>
-    </S.UpdateUser>
-  );
+  // return (
+  //   <S.UpdateUser>
+  //     <Spinner isLoading={isLoading} />
+  //     <S.H1>Admin Create User</S.H1>
+  //     <Form
+  //       onSubmit={res.handleSubmit((e) => setValues(e))}
+  //       error={hasError}
+  //       success={data?.success}
+  //     >
+  //       <Text
+  //         id="name"
+  //         placeholder="Name"
+  //         error={res.formState.errors.name?.message}
+  //         register={res.register('name')}
+  //       />
+  //       <Text
+  //         id="email"
+  //         placeholder="Email"
+  //         error={res.formState.errors.email?.message}
+  //         register={res.register('email')}
+  //       />
+  //       <Password
+  //         id="password"
+  //         placeholder="Password"
+  //         error={res.formState.errors.password?.message}
+  //         register={res.register('password')}
+  //       />
+  //       <Password
+  //         id="passwordConf"
+  //         placeholder="Password Confirmation"
+  //         error={res.formState.errors.passwordConf?.message}
+  //         register={res.register('passwordConf')}
+  //       />
+  //       <Checkbox value={role} onClick={() => setRole()}>
+  //         Admin
+  //       </Checkbox>
+  //       <S.Submit>Create User</S.Submit>
+  //     </Form>
+  //   </S.UpdateUser>
+  // );
+  return <div>cu</div>;
 };
 
 export const getServerSideProps = Admin();
